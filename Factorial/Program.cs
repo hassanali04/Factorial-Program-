9c2c8dd80fef5200b160abdae54d89dc1b48f8a3
hassanali04 <hassanali.exe5@gmail.com>
2023-11-03 03:43:57 +0500

    Factorial program

---
+++ Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace Factorial
+{
+    class Program
+    {
+        static void Main(string[] )
+        {
+            int num = 6;
+            int fact = 1;
+            for (int i = 1; i <= num; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine(fact);
+            Console.ReadLine();
+        }
+    }
+}
+
